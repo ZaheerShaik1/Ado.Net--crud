f04c5506d7be071c65c53da5ab802c6762de0d4e
ZaheerShaik1 <shaik.zaheer@veltris.com>
2024-09-25 15:08:02 +0530

    Performed ADO.NET CRUD Operations using MySQL and C# Application

---
+++ Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace EmployeeData
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.txtEmpName = new System.Windows.Forms.TextBox();
+            this.txtEmpID = new System.Windows.Forms.TextBox();
+            this.txtEmpAge = new System.Windows.Forms.TextBox();
+            this.txtEmpMobile = new System.Windows.Forms.TextBox();
+            this.txtEmpCity = new System.Windows.Forms.TextBox();
+            this.dgvEmployees = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmployees)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "EmployeeName";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Employee ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(116, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Age";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(100, 180);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Mobile";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(119, 215);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "City";
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(45, 335);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(103, 42);
+            this.btnInsert.TabIndex = 5;
+            this.btnInsert.Text = "INSERT";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(196, 335);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(103, 42);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "DELETE";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(45, 413);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(103, 42);
+            this.btnUpdate.TabIndex = 7;
+            this.btnUpdate.Text = "UPDATE";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(196, 413);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(103, 42);
+            this.btnLoad.TabIndex = 8;
+            this.btnLoad.Text = "LOAD";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // txtEmpName
+            // 
+            this.txtEmpName.Location = new System.Drawing.Point(154, 55);
+            this.txtEmpName.Name = "txtEmpName";
+            this.txtEmpName.Size = new System.Drawing.Size(175, 22);
+            this.txtEmpName.TabIndex = 9;
+            // 
+            // txtEmpID
+            // 
+            this.txtEmpID.Location = new System.Drawing.Point(154, 100);
+            this.txtEmpID.Name = "txtEmpID";
+            this.txtEmpID.Size = new System.Drawing.Size(175, 22);
+            this.txtEmpID.TabIndex = 10;
+            // 
+            // txtEmpAge
+            // 
+            this.txtEmpAge.Location = new System.Drawing.Point(154, 138);
+            this.txtEmpAge.Name = "txtEmpAge";
+            this.txtEmpAge.Size = new System.Drawing.Size(175, 22);
+            this.txtEmpAge.TabIndex = 11;
+            // 
+            // txtEmpMobile
+            // 
+            this.txtEmpMobile.Location = new System.Drawing.Point(154, 174);
+            this.txtEmpMobile.Name = "txtEmpMobile";
+            this.txtEmpMobile.Size = new System.Drawing.Size(175, 22);
+            this.txtEmpMobile.TabIndex = 12;
+            // 
+            // txtEmpCity
+            // 
+            this.txtEmpCity.Location = new System.Drawing.Point(154, 215);
+            this.txtEmpCity.Name = "txtEmpCity";
+            this.txtEmpCity.Size = new System.Drawing.Size(175, 22);
+            this.txtEmpCity.TabIndex = 13;
+            // 
+            // dgvEmployees
+            // 
+            this.dgvEmployees.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEmployees.Location = new System.Drawing.Point(395, 55);
+            this.dgvEmployees.Name = "dgvEmployees";
+            this.dgvEmployees.RowHeadersWidth = 51;
+            this.dgvEmployees.RowTemplate.Height = 24;
+            this.dgvEmployees.Size = new System.Drawing.Size(708, 272);
+            this.dgvEmployees.TabIndex = 14;
+            this.dgvEmployees.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvEmployees_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1168, 507);
+            this.Controls.Add(this.dgvEmployees);
+            this.Controls.Add(this.txtEmpCity);
+            this.Controls.Add(this.txtEmpMobile);
+            this.Controls.Add(this.txtEmpAge);
+            this.Controls.Add(this.txtEmpID);
+            this.Controls.Add(this.txtEmpName);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmployees)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.TextBox txtEmpName;
+        private System.Windows.Forms.TextBox txtEmpID;
+        private System.Windows.Forms.TextBox txtEmpAge;
+        private System.Windows.Forms.TextBox txtEmpMobile;
+        private System.Windows.Forms.TextBox txtEmpCity;
+        private System.Windows.Forms.DataGridView dgvEmployees;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace EmployeeData
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = "Server=localhost;Database=employeeData;Uid=root;Pwd=Zaheer@02;";
+
+        //string connectionString = @"Server=localhost;Initial catalog=employeeData;Integrated security=true;";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    string query = $"INSERT INTO employees (Name, Id, Age, Mobile, City) " +
+                                   $"VALUES ('{txtEmpName.Text}', {txtEmpID.Text}, '{txtEmpAge.Text}', '{txtEmpMobile.Text}','{txtEmpCity.Text}')";
+
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        command.ExecuteNonQuery();
+                        MessageBox.Show("Employee data inserted successfully!");
+                        ClearFields();
+                        LoadEmployeeData();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+
+        private void LoadEmployeeData()
+        {
+            selectFields();
+        }
+
+
+
+        // Fill the input fields when a row is selected in DataGridView
+        private void dgvEmployees_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dgvEmployees.Rows[e.RowIndex];
+                txtEmpID.Text = row.Cells["Id"].Value.ToString();
+                txtEmpName.Text = row.Cells["Name"].Value.ToString();
+                txtEmpAge.Text = row.Cells["Age"].Value.ToString();
+                txtEmpMobile.Text = row.Cells["Mobile"].Value.ToString();
+                txtEmpCity.Text = row.Cells["City"].Value.ToString();
+                //txtEmpPassword.Text = row.Cells["Password"].Value.ToString();
+
+            }
+        }
+
+        private void ClearFields()
+        {
+            txtEmpName.Text = "";
+            txtEmpID.Text = "";
+            txtEmpAge.Text = "";
+            txtEmpMobile.Text = "";
+            txtEmpCity.Text = "";
+            //txtEmpPassword.Text = "";
+        }
+
+        private void btnUpdate_Click_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnDelete_Click_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            selectFields();
+        }
+        public void selectFields()
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    string query = "SELECT * FROM employees";
+
+                    using (MySqlDataAdapter adapter = new MySqlDataAdapter(query, connection))
+                    {
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+                        dgvEmployees.DataSource = dataTable;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error loading data: " + ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    string query = $"DELETE FROM employees WHERE ID={txtEmpID.Text}";
+
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        command.ExecuteNonQuery();
+                        MessageBox.Show("Employee data deleted successfully!");
+                        ClearFields();
+                        LoadEmployeeData();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    string query = $"UPDATE employees SET Name='{txtEmpName.Text}', Age={txtEmpAge.Text}, Mobile='{txtEmpMobile.Text}', City='{txtEmpCity.Text}' WHERE Id='{txtEmpID.Text}'";
+
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        command.ExecuteNonQuery();
+                        MessageBox.Show("Employee data updated successfully!");
+                        
+                        LoadEmployeeData();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            LoadEmployeeData();
+
+        }
+    }
+}
